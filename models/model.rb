e816dd0d2f163545b8156963bfb09e33b8385d52
garendadourian <gardadour@gmail.com>
2015-08-27 18:24:04 +0000

    back end

--- models/model.rb
+++ models/model.rb
@@ -1,10 +1,79 @@
-class Result
-  def response(name, emotion)
-    if emotion == "panic" 
-      return "Don't worry!"
-    else
-      return "Me too!"
-    end
-    end 
-end
+class Today
 
+  def outfit (sex, temp, weather)
+    if temp >= 0 && temp < 45
+       clim = "cold"
+     elsif temp >= 45 && temp <= 65
+       clim = "moderate"
+     elsif temp > 65 && temp <= 100
+       clim = "hot"
+     end
+
+    if sex == "male"
+
+      if clim == "cold"
+
+        if weather == "sunny"
+          return
+
+        elsif weather == "rain"
+          return
+
+        elsif weather == "snow"
+          return
+      end
+
+      elsif clim == "moderate"
+
+        if weather == "sunny"
+          return
+        elsif weather == "rain"
+          return
+      end
+
+      elsif clim == "hot"
+
+        if weather == "sunny"
+          return
+
+        elsif weather == "rain"
+          return
+
+      end
+    end
+
+    else #female
+
+      if temp == "cold"
+
+        if weather == "sunny"
+          return
+
+        elsif weather == "rain"
+          return
+
+        elsif weather == "snow"
+          return
+      end
+
+      elsif clim == "moderate"
+
+        if weather == "sunny"
+          return
+        elsif weather == "rain"
+          return
+      end
+
+      elsif clim == "hot"
+
+        if weather == "sunny"
+          return
+
+        elsif weather == "rain"
+          return
+
+        end
+      end
+    end
+  end
+end
