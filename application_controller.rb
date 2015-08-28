f203a920e4f268587f4609ff349923d3683f9d4b
cpsheath <cpsheath@gmail.com>
2015-08-28 16:06:54 +0000

    finished

--- application_controller.rb
+++ application_controller.rb
@@ -1,3 +1,4 @@
+require_relative 'models/model.rb'
 require 'bundler'
 Bundler.require
 
@@ -6,4 +7,11 @@ class MyApp < Sinatra::Base
   get '/' do
     erb :index
   end
+  
+  post '/' do
+    day = Today.new
+    @imageurl = day.outfit(params[:sex], params[:quantity].to_i, params[:conditions])
+    erb :resultpage
+  end
+  
 end
--- models/model.rb
+++ models/model.rb
@@ -8,72 +8,38 @@ class Today
      elsif temp > 65 && temp <= 100
        clim = "hot"
      end
-
-    if sex == "male"
-
-      if clim == "cold"
-
-        if weather == "sunny"
-          return
-
-        elsif weather == "rain"
-          return
-
-        elsif weather == "snow"
-          return
-      end
-
-      elsif clim == "moderate"
-
-        if weather == "sunny"
-          return
-        elsif weather == "rain"
-          return
-      end
-
-      elsif clim == "hot"
-
-        if weather == "sunny"
-          return
-
-        elsif weather == "rain"
-          return
-
-      end
-    end
-
-    else #female
-
-      if temp == "cold"
-
-        if weather == "sunny"
-          return
-
-        elsif weather == "rain"
-          return
-
-        elsif weather == "snow"
-          return
-      end
-
-      elsif clim == "moderate"
-
-        if weather == "sunny"
-          return
-        elsif weather == "rain"
-          return
-      end
-
-      elsif clim == "hot"
-
-        if weather == "sunny"
-          return
-
-        elsif weather == "rain"
-          return
-
-        end
-      end
+    
+    if sex == "male" && clim == "cold" && weather == "sunny"
+      return "images/m-Sunny, 0-45.jpg"
+    elsif sex == "male" && clim == "cold" && weather == "rain"
+      return "images/m-Rain, 0-45.jpg"
+    elsif sex == "male" && clim == "cold" && weather == "snow"
+      return "images/m-0-45 snow.jpg"
+    elsif sex == "male" && clim == "moderate" && weather == "sunny"
+      return "images/m-Sunny, 46-65.jpg"
+    elsif sex == "male" && clim == "moderate" && weather == "rain"
+      return "images/m-46-65 rain.jpg"
+    elsif sex == "male" && clim == "hot" && weather == "sunny"
+      return "images/m-66-100 sunny.jpg"
+    elsif sex == "male" && clim == "hot" && weather == "rain"  
+      return "images/m-rain 66-100.jpg"
+    elsif sex == "female" && clim == "cold" && weather == "sunny"
+      return "images/Sunny, 0-45 degrees.jpg"
+    elsif sex == "female" && clim == "cold" && weather == "rain"
+      return "images/f-Rain, 0-45.jpg"
+    elsif sex == "female" && clim == "cold" && weather == "snow"
+      return "images/Snowy Weather.jpg"
+    elsif sex == "female" && clim == "moderate" && weather == "sunny"
+      return "images/46-65 sunnydegree weather.jpg"
+    elsif sex == "female" && clim == "moderate" && weather == "rain"
+      return "images/f-Rainy, 46-65.jpg"
+    elsif sex == "female" && clim == "hot" && weather == "sunny"
+      return "images/f-sunny 66-100.jpg"
+    elsif sex == "female" && clim == "hot" && weather == "rain"  
+      return "images/f-Rainy, 66-100 degrees.jpg"
+    else
+      return "images/snowman.jpg"
     end
   end
+    
 end
